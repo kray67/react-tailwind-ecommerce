c8f32879706b1f9b671251b3eef3a938bcd501ee
kray67 <jncdias@hotmail.com>
2021-12-09 13:28:56 +0000

    changes for build

--- src/components/CartTotals.js
+++ src/components/CartTotals.js
@@ -8,7 +8,7 @@ const CartTotals = (props) => {
     const tax = (subTotal * 0.0975)
     const total = subTotal + tax
     return (
-        <div className="totals flex flex-col w-5/6 pt-5 border-t-4 border-gray-300 border-solid">
+        <div className="totals flex flex-col gap-y-2 w-5/6 pt-5 border-t-2 border-gray-300 border-solid">
             <div className="w-3/4 self-end flex items-baseline justify-end">
                 <div className="w-2/5 font-bold text-xl text-right">Subtotal:</div>
                 <div className="w-3/5 font-bold text-4xl text-right"> ${subTotal.toFixed(2)} </div>
@@ -19,7 +19,7 @@ const CartTotals = (props) => {
             </div>
             <div className="w-3/4 self-end flex items-baseline justify-end">
                 <div className="w-2/5 font-bold text-xl text-right">Total:</div>
-                <div className="w-3/5 font-bold text-4xl text-right"> ${total.toFixed(2)} </div>
+                <div className="w-3/5 font-bold text-4xl text-right text-purple-600"> ${total.toFixed(2)} </div>
             </div>
         </div>
     )
